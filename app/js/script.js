8da4a99186fdb3bec61fa321e90129eed55fcc54
Jacob Schaefer <pyremell@gmail.com>
2021-12-09 13:47:22 -0800

    Coded mobile menu modal, and adjusted breakpoint for tablet to include mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,24 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function() {
 
   if (header.classList.contains('open')) {
-    header.classList.remove('open');
+    body.classList.remove('noscroll'); // Prevents scrolling while mobile menu is open
+    header.classList.remove('open'); // Close hamburger menu
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
   }
   else {
+    body.classList.add('noscroll');
     header.classList.add('open');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   }
 })
