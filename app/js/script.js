8883593a125c94354fc87c0fea532350c02f68eb
Jacob Schaefer <pyremell@gmail.com>
2021-12-08 21:15:29 -0800

    Built animated mobile hamburger menu button and made it hidden on tablet/desktop

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,13 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function() {
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+  }
+  else {
+    header.classList.add('open');
+  }
+})
